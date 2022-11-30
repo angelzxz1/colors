0a851656309ead9e1401ae55364f4be39f18851a
Angel Zúñiga Navarro <angel.zuna@icloud.com>
2022-11-30 23:37:02 +0000

    Update colorGenerator.ts and theme.ts

--- lib/theme.ts
+++ lib/theme.ts
@@ -3,26 +3,26 @@ import { mode } from "@chakra-ui/theme-tools";
 import type { StyleFunctionProps } from "@chakra-ui/styled-system";
 import colors from "./colors";
 const config = {
-  initialColorMode: "dark",
-  useSystemColorMode: false,
-  disableTransitionOnChange: false,
+	initialColorMode: "dark",
+	useSystemColorMode: false,
+	disableTransitionOnChange: false,
 };
 
 const styles = {
-  global: (props: StyleFunctionProps) => ({
-    body: {
-      // fontFamily: "body",
-      // color: mode("gray.800", "whiteAlpha.900")(props),
-      // bg: mode("white", "gray.800")(props),
-      // lineHeight: "base",
-    },
-  }),
+	global: (props: StyleFunctionProps) => ({
+		body: {
+			// fontFamily: "body",
+			// color: mode("gray.800", "whiteAlpha.900")(props),
+			// bg: mode("white", "gray.800")(props),
+			// lineHeight: "base",
+		},
+	}),
 };
 
 const theme = extendTheme({
-  config,
-  styles,
-  colors: { colors },
+	config,
+	styles,
+	colors: { colors },
 });
-// console.log(theme);
+console.log(theme);
 export default theme;
